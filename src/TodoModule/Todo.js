64c4a9d5c5a1168ee7ac72174968a68c9b66d9de
Anna <anna.russkova@gmail.com>
2022-06-16 19:09:44 +0200

    final version

--- src/TodoModule/Todo.js
+++ src/TodoModule/Todo.js
@@ -17,11 +17,10 @@ const date = () => {
 const Actions = {
     CREATED: "CREATED",
     UPDATED: "UPDATED",
-    DELETED: "DELETED",
+    COMPLETED: "COMPLETED",
 };
-
 class HistoryItemDTO {
-    constructor(field, action = Actions.CREATED, prevValue = "", currentValue, appliedAt) {
+    constructor(field, action = "", prevValue = "", currentValue, appliedAt) {
         this.id = uuidv4();
         this.field = field;
         this.action = action;
@@ -30,30 +29,29 @@ class HistoryItemDTO {
         this.appliedAt = appliedAt;
     }
 }
-
 class TodoItemDTO {
     constructor(title, description) {
         this.id = uuidv4();
         this.title = title;
         this.description = description;
         this.completed = false;
-        this.history = [new HistoryItemDTO('title', Actions.CREATED, "", title, date())]; // Date.now()
+        this.history = [new HistoryItemDTO('title', Actions.CREATED, "", title, date())]; 
     }
 }
-
 
 export class Todo extends Component {
     state = {
         list: [
             new TodoItemDTO('Water Plants', 'in the kitchen'),
             new TodoItemDTO('Pack the bag', 'preparation for trip to London'),
-            new TodoItemDTO('Charge laptop', 'preparation for trip to Kherson'),
+            new TodoItemDTO('Charge laptop', 'preparation for trip to London'),
+            new TodoItemDTO('Make backup of HDD', 'Use TimeMachine for that'),
+            new TodoItemDTO('Finish my Project', 'Use React'),
         ],
         filter: '',
         selectedItem: null,
     };
 
-    // DONE ✅
     onItemCompleted = (id) => {
         let prevTodoItem = this.state.list.find(item => item.id === id);
         let updatedItem = { ...prevTodoItem, completed: !prevTodoItem.completed };
@@ -71,7 +69,6 @@ export class Todo extends Component {
         });
     }
 
-    // DONE ✅
     onItemSelected = (id) => {
         let todoItem = this.state.list.find(item => item.id === id);
         this.setState({ selectedItem: todoItem });
@@ -80,50 +77,55 @@ export class Todo extends Component {
     todoAddHandler = (newItem) => {
         this.setState({
             list: [...this.state.list, newItem]
-        },
-            // ()=> console.log(this.state)
-        )
+        })
     }
 
     onFormSubmitHandler = (event) => {
         event.preventDefault();
+        if(this.state.title !== ' ' && this.state.title !== ''){
+            
+            this.setState({
+                selectedItem: false
+            })
 
-        this.setState({
-            list: this.state.list.map(item => item.selected = false)
-        })
-
-        let newItem = new TodoItemDTO(this.state.title, 'This is default Description');
-        this.todoAddHandler(newItem);
+            let newItem = new TodoItemDTO(this.state.title, 'This is default Description');
+            this.todoAddHandler(newItem); 
+        }
     }
 
     onTitleChangeHandler = ({ target }) => {
         this.setState(
-            { list: [...this.state.list], title: target.value }
+            { list: [...this.state.list], title: target.value },
+            // console.log(this.state)
         )
     }
 
     onDescrSubmitHandler = (event) => {
         event.preventDefault();
         this.state.list.filter(item => {
-            if (item.selected && this.state.updatedDescr && this.state.updatedDescr !== item.description) {
+            if (this.state.selectedItem.id === item.id && this.state.updatedDescr && this.state.updatedDescr !== item.description) {
                 let prevValueDescr = item.description;
                 item.description = this.state.updatedDescr;
 
-                let newHistoryRecord = { field: 'description', action: `Changed task description from "${prevValueDescr}" to "${item.description}"`, prevValue: prevValueDescr, currentValue: this.state.updatedDescr, appliedAt: this.state.timeChange }
-                item.history.push(newHistoryRecord);
-                console.log(item.history);
-                return this.setState({ list: [...this.state.list] });
-
+                item.history.push( new HistoryItemDTO(
+                        'description',
+                        Actions.UPDATED,
+                        prevValueDescr,
+                        this.state.updatedDescr,
+                        date(),
+                ))
+                return this.setState({ list: [...this.state.list], updatedDescr: null });
             } else {
                 return item.description;
             }
-        });
+        },
+        //  console.log(this.state)
+        );
     }
 
     onDescrChangeHandler = ({ target }) => {
         this.setState(
-            { list: [...this.state.list], updatedDescr: target.value, timeChange: date() },
-            // ()=> console.log(this.state)
+            { list: [...this.state.list], updatedDescr: target.value}
         )
     }
 
@@ -138,20 +140,21 @@ export class Todo extends Component {
     };
 
     render() {
-
         return (
             <div className='todo-container'>
                 <div className='todo-containerComponents'>
                     <h1 className='todo-title'>Todos</h1>
                     <TodoSearchBar onSearchTitleFilter={this.onSearchTitleFilter} />
 
-                    {this.getFilteredItems().map(item => <TodoItem
-                        key={item.id}
-                        item={item}
-                        selected={this.state.selectedItem ? item.id === this.state.selectedItem.id : false}
-                        onItemCompleted={this.onItemCompleted}
-                        onItemSelected={this.onItemSelected}
-                    />)}
+                    <div className="todo-list">
+                        {this.getFilteredItems().map(item => <TodoItem
+                            key={item.id}
+                            item={item}
+                            selected={this.state.selectedItem ? item.id === this.state.selectedItem.id : false}
+                            onItemCompleted={this.onItemCompleted}
+                            onItemSelected={this.onItemSelected}
+                        />)}
+                    </div>
 
                     <TodoForm
                         item={this.state.list}
@@ -160,14 +163,6 @@ export class Todo extends Component {
                         todoAddHandler={this.todoAddHandler} />
                 </div>
                 <div className='todo-presentationComponents'>
-
-                    {/* {this.state.list.map(item => item.selected &&
-                        <TodoItemInfo
-                            key={item.id}
-                            item={item}
-                            onDescrChangeHandler={this.onDescrChangeHandler}
-                            onDescrSubmitHandler={this.onDescrSubmitHandler} />
-                    )} */}
 
                     {this.state.selectedItem
                         ? <TodoItemInfo
--- src/TodoModule/components/PresentationComponents/HistoryItem/HistoryItem.js
+++ src/TodoModule/components/PresentationComponents/HistoryItem/HistoryItem.js
@@ -1,9 +1,24 @@
 import { Component } from "react";
 import './HistoryItem.css';
 
+const currentview = (value) =>{
+    if(value === true){
+        return 'completed';
+    } else if(value === false){
+        return 'not completed';
+    } else {
+        return value;
+    } 
+}
 export class HistoryItem extends Component {
     convertToReadableHistoryView = (historyItem) => {
-        return `Що ж, станом на ${historyItem.appliedAt} було ${historyItem.action === "CREATED" ? 'створено' : 'оновлено'} ${historyItem.prevValue}`
+        if(historyItem.action === ''){
+            return null
+        } else{
+            return `Todo-item ${historyItem.action === "CREATED" 
+            ? 'was created as' 
+            : 'was updated to'} "${currentview(historyItem.currentValue)}"`
+        }
     };
 
     render() {
@@ -11,13 +26,9 @@ export class HistoryItem extends Component {
             <>
                 <div className="historyItem">
                     <div className={this.props.historyItem.action ? "historyItem-line" : null}></div>
-                    <div className="historyItem-info">
-                        
+                    <div className="historyItem-info">  
                         <div className="historyItem-text">{this.convertToReadableHistoryView(this.props.historyItem)}</div>
-                        <div className="historyItem-text">{this.props.historyItem.prevValue}</div>
-                        <div className="historyItem-text">{this.props.historyItem.currentValue}</div>
                         <div className="historyItem-time">{this.props.historyItem.appliedAt}</div>
-
                     </div>
                 </div>   
             </>
